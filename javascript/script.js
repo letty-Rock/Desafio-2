379aab8d798d4f2e5e1a1b7187bc197674b23905
letty-Rock <leticiasantos.roque@outlook.com>
2021-07-21 19:34:13 -0300

    pause

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,52 @@
-const form = document.getElementById('form')
+function validarProduto(idproduto, idcodigo, idquantidade) {
+  let produto = document.getElementById(idproduto).value
+  let codigo = document.getElementById(idcodigo).value
+  let quantidade = document.getElementById(idquantidade).value
+  if (produto == '')
+    alert(' Nome do produto não pode estar em branco. Favor preenchê-lo!')
+  else if (codigo == '')
+    alert(' Código do produto não pode estar em branco. Favor preenchê-lo!')
+  else cadastrarProduto(nome, codigo, parseInt(quantidade))
+}
+function cadastrarProduto(produto, codigo, quantidade) {
+  let novoProduto = { nome: produto, codigo: codigo, quantidade: quantidade }
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
+  if (typeof Storage !== 'undefined') {
+    let produtos = localStorage.getItem('produtos')
+    if (produtos == null) produtos = []
+    else produtos = JSON.parse(produtos)
+    produtos.push(novoProduto)
+    localStorage.setItem('produtos', JSON.stringify(produtos))
+    alert(
+      'Foram cadastradas com sucesso' +
+        quantidade +
+        'unidades do produto' +
+        produto +
+        '!'
+    )
+    atualizarTotalEstoque('totalEstoque')
+    location.reload()
+  } else
+    alert(
+      'A versão do seu navegador é muito antiga. Por isso, não será possível executar essa aplicação'
+    )
+}
 
-  let produto = document.getElementById('produto').value
-  let quantidade = document.getElementById('quantidade').value
-  let codigo = document.getElementById('codigo').value
-  let data = {
-    produto,
-    quantidade,
-    codigo
+function atualizarTotalEstoque(idCampo) {
+  localStorage.setItem(
+    'totalEstoque',
+    ++document.getElementById(idCampo).innerHTML
+  )
+}
+function carregarTotalEstoque(idCampo) {
+  if (typeof Storage !== 'undefined') {
+    let totalEstoque = localStorage.getItem('totalEstoque')
+    if (totalEstoque == null) totalEstoque = 0
+    document.getElementById(idCampo).innerHTML = totalEstoque
   }
-  let convertData = JSON.stringify(data)
-  localStorage.setItem('lead', convertData)
-}) /*
-console.log(`Produto ${produto} cadastrado`)*/
+  alert(
+    'A versão do seu navegador é muito antiga. Por isso, não será possível executar essa aplicação'
+  )
+}
+
+function listarEstoque() {}
